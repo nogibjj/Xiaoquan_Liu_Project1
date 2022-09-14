651ce0610b6ed6efcec960fa9980cae02807a5fa
XquanL <112578003+XquanL@users.noreply.github.com>
2022-09-14 19:55:39 +0000

    cli-query

--- dblib/querydb.py
+++ dblib/querydb.py
@@ -5,7 +5,8 @@ def querydb(query="SELECT * FROM default.diamonds LIMIT 2"):
     with sql.connect(
         server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
         http_path=os.getenv("DATABRICKS_HTTP_PATH"),
-        access_token=os.getenv("DATABRICKS_TOKEN"),) as connection:
+        access_token=os.getenv("DATABRICKS_TOKEN"),
+        ) as connection:
     
         with connection.cursor() as cursor:
             cursor.execute(query)
