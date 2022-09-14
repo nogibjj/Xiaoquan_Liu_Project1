89ee71bc60be8a01f675dde611593a167e53ca87
XquanL <112578003+XquanL@users.noreply.github.com>
2022-09-14 16:40:11 -0400

    Update hello_query_sql_db.py

--- hello_query_sql_db.py
+++ hello_query_sql_db.py
@@ -13,7 +13,7 @@ def cli():
 @cli.command()
 @click.option(
     "--query",
-    default="SELECT * FROM diabetes LIMIT 3",
+    default="SELECT * FROM health_indicator_diabetes LIMIT 3",
     help="SQL query to execute",
 )
 def cli_query(query):
